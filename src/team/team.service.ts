3cc144c4c5a1dda48fbbdd6fb701e1450ee20251
Fred Lavoie <flavoie3@gmail.com>
2022-12-19 17:36:43 -0500

    Add some validation to team create and update

--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { Team } from '@prisma/client';
 import { NewTeam, UpdateTeam } from 'src/graphql';
@@ -23,6 +23,27 @@ export class TeamService {
 
   // Create a team
   async createTeam(input: NewTeam): Promise<Team> {
+    if (input.pokemonIds.length > 5) {
+      throw new HttpException(
+        'Team size must be 5 or less',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    this.prisma.pokemon
+      .findMany({
+        where: {
+          id: { in: input.pokemonIds.map((ea) => parseInt(ea)) },
+        },
+      })
+      .then((data) => {
+        if (data.length !== input.pokemonIds.length) {
+          throw new HttpException(
+            "One or more pokemon doesn't exit",
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+      });
     return this.prisma.team.create({
       data: input,
     });
@@ -30,6 +51,28 @@ export class TeamService {
 
   // Update a team
   async updateTeam(params: UpdateTeam): Promise<Team> {
+    if (params.pokemonIds.length > 5) {
+      throw new HttpException(
+        'Team size must be 5 or less',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    this.prisma.pokemon
+      .findMany({
+        where: {
+          id: { in: params.pokemonIds.map((ea) => parseInt(ea)) },
+        },
+      })
+      .then((data) => {
+        if (data.length !== params.pokemonIds.length) {
+          throw new HttpException(
+            "One or more pokemon doesn't exit",
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+      });
+
     return this.prisma.team.update({
       where: {
         id: parseInt(params.id),
